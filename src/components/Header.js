f2ad75fc3e29b4208043801432a788c2c3f32812
Giovanni Toscani <gtoscani@and.digital>
2018-04-06 16:44:26 +0100

    Added the Header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="header">
+    <img className="logo" src="/img/logo.svg" alt="logo" title="logo" height="200" width="200" />
+    <h1 className="logo-title">Daily Food Planner</h1>
+  </div>
+);
+
+export default Header;
