4ee67f40ce7cb2bab317d129c3dc35182c265975
Giovanni Toscani <gtoscani@and.digital>
2018-04-17 13:36:19 +0100

    Added responsive CSS styles and calories label

--- src/components/MealPlannerInput.js
+++ src/components/MealPlannerInput.js
@@ -36,8 +36,7 @@ class MealPlannerInput extends React.Component {
               type="text"
             /><br />
             <span className="form-input-info">
-              Example: nuts, shallots - separated by comma.<br />
-              Leave blank if you don&apos;t want to exclude anything
+              Example: nuts, shallots - separated by comma. Leave blank if you don&apos;t want to exclude anything
             </span>
           </div>
           <div className="input-container">
--- src/components/MealResultsListing.js
+++ src/components/MealResultsListing.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import MealCard from './MealCard';
 import '../css/components/MealResultsListing.css';
+import '../css/components/MealCard.css';
 
 class MealResultsListing extends React.Component {
   constructor(props) {
@@ -10,69 +12,18 @@ class MealResultsListing extends React.Component {
  
   render() {
     return (
-      <div className="meal-results-listing">
-
-        <div className="singleColumn">
-          <h2 className="mealTitle">Breakfast</h2>
-          <div className="imgContainer">
-            <img className="imageRecipe" src="/img/healthy-food-1.jpg" alt="Breakfast-recipe" title="Breakfast-recipe"  />
-            <a href="#"><div className="hoverBox">
-              <h3 className="hoverTitle">Time to cook!</h3>
-              <img className="imgCooking" src="/img/cooking.svg" alt="Time" title="Time" height="60" width="60" />
-            </div></a>
-          </div>
-          <div className="mealDetailsContainer">
-            <div className="mealDetails">
-              <h3 className="recipeTitle">Pastasciutta</h3>
-            </div>
-            <div className="mealDetails clockDiv">
-              <img className="imgClock" src="/img/clock.svg" alt="Time" title="Time" height="20" width="20" />
-              <h3 className="recipeDuration">49'</h3>
-            </div>
-          </div>
-        </div>
-
-        <div className="singleColumn">
-          <h2 className="mealTitle">Lunch</h2>
-          <div className="imgContainer">
-            <img className="imageRecipe" src="/img/healthy-food-2.jpg" alt="Breakfast-recipe" title="Breakfast-recipe"  />
-            <div className="hoverBox">
-              <h3 className="hoverTitle">Time to cook!</h3>
-              <img className="imgCooking" src="/img/cooking.svg" alt="Time" title="Time" height="60" width="60" />
-            </div>
-          </div>
-          <div className="mealDetailsContainer">
-            <div className="mealDetails">
-              <h3 className="recipeTitle">Lasagna</h3>
-            </div>
-            <div className="mealDetails clockDiv">
-              <img className="imgClock" src="/img/clock.svg" alt="Time" title="Time" height="20" width="20" />
-              <h3 className="recipeDuration">34'</h3>
-            </div>
-          </div>
-        </div>
-        
-        <div className="singleColumn">
-          <h2 className="mealTitle">Dinner</h2>
-          <div className="imgContainer">
-            <img className="imageRecipe" src="/img/healthy-food-3.jpg" alt="Breakfast-recipe" title="Breakfast-recipe"  />
-            <div className="hoverBox">
-              <h3 className="hoverTitle">Time to cook!</h3>
-              <img className="imgCooking" src="/img/cooking.svg" alt="Time" title="Time" height="60" width="60" />
-            </div>
-          </div>
-          <div className="mealDetailsContainer">
-            <div className="mealDetails">
-              <h3 className="recipeTitle">Vegan Pizza</h3>
-            </div>
-            <div className="mealDetails clockDiv">
-              <img className="imgClock" src="/img/clock.svg" alt="Time" title="Time" height="20" width="20" />
-              <h3 className="recipeDuration">27'</h3>
-            </div>
-          </div>
-        </div>
-
+    <div className="resultsContainer">
+      <div className="mealCaloriesLabel">
+        <h3 className="h3Calories">Total day calories: <span className="caloriesAmount">2800</span></h3>
       </div>
+      <div className="meal-results-listing">
+        <MealCard />
+        <MealCard />
+        <MealCard />
+      </div>
+
+      
+    </div>
     );
   }
 }
