6c568b31b86c1cc5ccb87b43f9603efc0b1b6966
ibartolovic <ibartolovic@and.digital>
2018-04-19 10:40:45 +0100

    Working API with certain inputs
    --Key not hidden. Need to create .env file with the key inside and uncomment the // const API_KEY = process.env.REACT_APP_API_KEY; in app.js

--- src/App.js
+++ src/App.js
@@ -4,28 +4,28 @@ import Header from './components/Header';
 import MealPlannerInput from './components/MealPlannerInput';
 import MealResultsListing from './components/MealResultsListing';
 
+// const API_KEY = process.env.REACT_APP_API_KEY;
 
-//var mykey = config.MY_KEY;
-const API_KEY = process.env.REACT_APP_API_KEY;
-
-/*var request = require("request");
+var request = require("request");
 
 var options = { method: 'GET',
-  url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/324694/analyzedInstructions',
+  url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/mealplans/generate?diet=vegetarian&exclude=shellfish%2C+olives&targetCalories=2000&timeFrame=day',
   qs: { stepBreakdown: 'false' },
   headers:
    { 'Postman-Token': 'fd97f5de-ff8a-4f54-83c9-9a261d2e7e71',
      'Cache-Control': 'no-cache',
-     'X-Mashape-Host': 'spoonacular-recipe-food-nutrition-v1.p.mashape.com',
-     'X-Mashape-Key': API_KEY}}
+     'X-Mashape-Host': 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/mealplans/generate?timeFrame=day',
+     'X-Mashape-Key': '7cIzGZIM9vmshQSJGXgk49636kCIp1DLKmpjsntDIdeyrLkvSX'}}
      ;
 
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
 
-  console.log(body);
+  const newBody = JSON.parse(body)
+  console.log(newBody);
+
 });
-*/
+
 
 const App = () => (
   <div className="App">
