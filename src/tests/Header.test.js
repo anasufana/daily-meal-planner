1ac7ca4d4d3bea2931890d775cf3955ad699c83b
anasufana <asufana@and.digital>
2018-05-01 11:23:39 +0100

    Header Shapshot Test

---
+++ src/tests/Header.test.js
@@ -0,0 +1,11 @@
+/* global it, expect */
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Header from '../components/Header';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Header />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
